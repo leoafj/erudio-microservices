012cf923a007c2caf419a1c3284572908bee9cdb
Leonardo Freitas <leoafj@hotmail.com>
2022-04-04 16:51:18 -0300

    Dockerizando o microsserviço book-service

---
+++ Dockerizing/book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -0,0 +1,50 @@
+package br.com.erudio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Book;
+import br.com.erudio.proxy.CambioProxy;
+import br.com.erudio.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Book endpoint")
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@Autowired
+	private CambioProxy proxy;
+	
+	@Operation(summary = "Find a specific book by your ID")
+	@GetMapping(value = "/{id}/{currency}")
+	public Book findBook(
+			@PathVariable("id") Long id,
+			@PathVariable("currency") String currency
+			) {
+		
+		var book = repository.getById(id);
+		if (book == null) throw new RuntimeException("Book not Found");
+		
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+				
+		var port = environment.getProperty("local.server.port");
+		book.setEnvironment(
+				"Book Port:" + port + 
+				" Cambio Port" + cambio.getEnviroment());
+		book.setPrice(cambio.getConvertedValue());
+		return book;	
+	}
+	
+}
